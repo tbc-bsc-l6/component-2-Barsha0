abae3d3a80ad1596502e774f60aff6fdafd0674a
Barsha Dhakal <dhakalbarsha519@gmail.com>
2023-12-30 23:21:57 +0545

    header and footer are created as a different files.

--- resources/views/home/homepage.blade.php
+++ resources/views/home/homepage.blade.php
@@ -755,45 +755,7 @@
 
 	<!--================ start footer Area  =================-->
 	<footer class="footer-area section-gap">
-		<div class="container">
-			<div class="single-footer-widget footer_middle">
-				{{-- <img src="img/logo.png" alt=""> --}}
-				<p class="mt-50">Stay updated with our latest trends</p>
-				<div id="mc_embed_signup">
-					<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-					 method="get" class="subscribe_form relative">
-						<div class="input-group d-flex flex-row">
-							<input name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '"
-							 required="" type="email">
-							<button class="btn sub-btn">
-								<span class="lnr lnr-arrow-right"></span>
-							</button>
-						</div>
-						<div class="mt-10 info"></div>
-					</form>
-				</div>
-			</div>
-			<div class="footer-bottom footer_copy">
-				<div class="footer-social">
-					<a href="#">
-						<i class="fa fa-facebook"></i>
-					</a>
-					<a href="#">
-						<i class="fa fa-twitter"></i>
-					</a>
-					<a href="#">
-						<i class="fa fa-dribbble"></i>
-					</a>
-					<a href="#">
-						<i class="fa fa-behance"></i>
-					</a>
-				</div>
-
-				<p class="col-lg-12 footer-text"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-			</div>
-		</div>
+		@include('home.footer')
 	</footer>
 	<!--================ End footer Area  =================-->
 
